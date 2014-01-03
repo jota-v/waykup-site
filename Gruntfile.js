ceceb0ff1919dd8aee697f2f12d51a6a8dc1a936
jota-v <juanvanni@gmail.com>
2014-01-02 23:02:02 -0300

    fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -203,8 +203,7 @@ module.exports = function (grunt) {
                     src: [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
                         '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{gif,jpeg,jpg,png,webp}',
-                        '<%= yeoman.dist %>/styles/fonts/{,*/}*.*'
+                        '<%= yeoman.dist %>/images/{,*/}*.{gif,jpeg,jpg,png,webp}'
                     ]
                 }
             }
