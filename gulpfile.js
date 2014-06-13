cf04a29aebc69cbf103f863dbc06be67fd32db8f
jota-v <juanvanni@gmail.com>
2014-06-12 22:47:56 -0300

    correccion link y media query rateus

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ gulp.task('fonts', function () {
 });
 
 gulp.task('extras', function () {
-    return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
+    return gulp.src(['app/*.*', '!app/*.html', 'app/CNAME'], { dot: true })
         .pipe(gulp.dest('dist'));
 });
 
